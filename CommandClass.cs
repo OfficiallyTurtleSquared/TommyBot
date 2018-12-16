eb97c3294f9d6c5e817d20f2ada76fc2f8a0049f
OfficiallyTurtleSquared <36559587+OfficiallyTurtleSquared@users.noreply.github.com>
2018-12-16 01:27:01 -0500

    Removed Commented code to clean up CommandClass.cs
    Fixes an issue that was previously solved with 27 different "if" statements.
    
    Beautiful, I know.

--- CommandClass.cs
+++ CommandClass.cs
@@ -40,38 +40,6 @@ namespace TommyBot.Modules
             int index = new Random().Next(0, filegrab.Length);
 
             await Context.Channel.SendFileAsync($"{filelist}\\{filegrab[index].Name}");
-
-
-            /* Random rnd = new Random();
-            int song = rnd.Next(1, 27);
-
-            if (song == 1) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Budenny's March.mp3");
-            if (song == 2) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Does the Russian Want the War.mp3");
-            if (song == 3) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\In the Forest, Near the Frontline.mp3");
-            if (song == 4) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Katiusha.mp3");
-            if (song == 5) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\March of the defenders of Moscow.mp3");
-            if (song == 6) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Moscow Defender's Song.mp3");
-            if (song == 7) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\My Moscow.mp3");
-            if (song == 8) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\National Anthem of USSR.mp3");
-            if (song == 9) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Our Power.mp3");
-            if (song == 10) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir - The song about the 27th division.mp3");
-            if (song == 11) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir - Victory Day.mp3");
-            if (song == 12) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir - We Are The Red Cavalry.mp3");
-            if (song == 13) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir - When The Soldiers Sing.mp3");
-            if (song == 14) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir Cossack's Song..mp3");
-            if (song == 15) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir Oh Fields, My fields..mp3");
-            if (song == 16) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir On the Road..mp3");
-            if (song == 17) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir Partisan's Song..mp3");
-            if (song == 18) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Red Army Choir The Red Army Is the Strongest..mp3");
-            if (song == 19) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Spring of 1945.mp3");
-            if (song == 20) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\The Horse Army Song.mp3");
-            if (song == 21) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\The Parade's Song.mp3");
-            if (song == 22) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\The Party's Song.mp3");
-            if (song == 23) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\The Red Army Choir Sacred War..mp3");
-            if (song == 24) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\The Russian Walk.mp3");
-            if (song == 25) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Toula's Defenders Song.mp3");
-            if (song == 26) await Context.Channel.SendFileAsync(@"C:\Users\TurtleSquared\Documents\Visual Studio 2017\Projects\OperationRussianElon\OperationRussianElon\RedChoir\Varshavianka.mp3");
-            if (song == 27) await Context.Channel.SendFileAsync(@"");*/
         }
     }
     public class SayCommand : ModuleBase<SocketCommandContext>
