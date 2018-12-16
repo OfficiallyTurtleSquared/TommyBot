da4d5b1565380c5ddf2039b03521d808519b3ab7
OfficiallyTurtleSquared <36559587+OfficiallyTurtleSquared@users.noreply.github.com>
2018-12-16 00:44:06 -0500

    Initial Commit
    Initial upload of all code-based files for TommyBot

---
+++ CommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord.Rest;
+using Discord;
+using System.Diagnostics;
+
+
+namespace TommyBot
+{
+    public class CommandHandler
+    {
+        private DiscordSocketClient _client;
+
+        private CommandService _service;
+
+
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _service = new CommandService();
+
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+
+            int argPos = 0;
+            if (msg.HasCharPrefix('?', ref argPos) && msg.Author.IsBot == false)
+            {
+                var result = await _service.ExecuteAsync(context, argPos);
+
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Audio;
+using Discord.Net;
+using Discord.Rest;
+using Discord.Webhook;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TommyBot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+            new CommandHandler();
+
+            await _client.LoginAsync(TokenType.Bot, "NDcwMzkzOTI4MzgyNjc3MDAy.DtesQA.zXxE6jutPc8rfrmgfdwW0EiiYXU");
+            await _client.SetStatusAsync(UserStatus.Online);
+            await _client.StartAsync();
+            _handler = new CommandHandler();
+            await _handler.InitializeAsync(_client);
+            Console.WriteLine("TommyBot V0.1");
+            Console.WriteLine("Ready.");
+            Console.ForegroundColor = ConsoleColor.White;
+            System.Threading.Thread.Sleep(2000);
+            await _client.SetGameAsync($"Glory to Elon-san!");
+            await Task.Delay(-1);
+
+        }
+
+    }
+}
